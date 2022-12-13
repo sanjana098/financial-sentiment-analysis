c5834a103ec28881beb8b6ef08d78f99c3844abd
sanjana098 <sanjana22021998@gmail.com>
2022-12-13 14:18:26 -0800

    fix the nltk package download issue

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import numpy as np
 import tensorflow as tf
 from keras.utils import pad_sequences
 
+import ssl
+import nltk
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('wordnet')
+
 from nltk.stem import WordNetLemmatizer 
 
 
